a3eea7df2d6b6eae049d3a70daac3b29a2798a5c
salauddin <snalband@thoughtworks.com>
2017-01-05 18:45:15 +0530

    Added basic gradle project structurce

---
+++ src/main/java/com/myworld/connect/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.myworld.connect.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Map<String, Object> model) {
+        return "index";
+    }
+}
