0fd3f32f16049bcce1c9c8113269b4305be0bcb4
salauddin <snalband@thoughtworks.com>
2017-01-09 18:11:57 +0530

    Added Spring Support

--- src/main/java/com/myworld/connect/controllers/HomeController.java
+++ src/main/java/com/myworld/connect/controllers/HomeController.java
@@ -3,14 +3,13 @@ package com.myworld.connect.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Map;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Map<String, Object> model) {
-        return "index";
+    public ModelAndView login() {
+        return new ModelAndView("index");
     }
 }
